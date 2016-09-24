5a2830112eef99c410351ba10699c442b153308e
Emily Herbert <eherbert@trinity.edu>
2016-09-24 17:48:47 -0500

    Seperating "Scene" and "2DFractal" scenes

--- Assets/Resources/Scripts/Network/GameManager.cs
+++ Assets/Resources/Scripts/Network/GameManager.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : Photon.PunBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public static GameManager instance;
+
+    // Use this for initialization
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("Fractals");
+    }
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        instance = this;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void JoinGame()
+    {
+        RoomOptions ro = new RoomOptions();
+        ro.maxPlayers = 6;
+        PhotonNetwork.JoinOrCreateRoom("Default Room", ro, null);
+    }
 }
