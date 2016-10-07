5f46fcd633ff511379eb66f57a1dc2b3b206b417
Emily Herbert <eherbert@trinity.edu>
2016-10-07 01:48:59 -0500

    ant movement

---
+++ Assets/Resources/Scripts/BodyFollowScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BodyFollowScript : MonoBehaviour {
+
+    public Transform player;
+    public float chaseSpeed = 5f;
+    public float stopDistance = 5f;
+    public float currentSpeed;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(transform.position, player.position) >= stopDistance) // check the distance between this game object and Player and continue if it's less than Range
+        {
+            currentSpeed = chaseSpeed * Time.deltaTime; // set the CurrentSpeed to ChaseSpeed and multiply by Time.deltaTime (this prevents it from moving based on FPS)
+            transform.position = Vector3.MoveTowards(transform.position, player.position, currentSpeed);  // set this game objects position to the Player's position at the speed of CurrentSpeed
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+using UnityEngine.UI;
+
+
+
+public class Player : MonoBehaviour{
+
+    public float walkSpeed;
+
+    private Rigidbody2D theRigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        theRigidBody = transform.GetChild(0).gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        theRigidBody.velocity = new Vector2(inputX * walkSpeed, inputY * walkSpeed);
+    }
+}
