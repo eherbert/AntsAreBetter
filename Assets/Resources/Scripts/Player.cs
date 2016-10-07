983334ec1b02b788779d871d04949376f10da3f9
Emily Herbert <eherbert@trinity.edu>
2016-10-07 14:17:59 -0500

    ant random movement

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -9,20 +9,38 @@ using UnityEngine.UI;
 public class Player : MonoBehaviour{
 
     public float walkSpeed;
+    public float newDesiredVector2Threshold;
+    public bool playerControlled;
 
     private Rigidbody2D theRigidBody;
+    private Vector2 desiredVector2;
 
     // Use this for initialization
     void Start()
     {
-        theRigidBody = transform.GetChild(0).gameObject.GetComponent<Rigidbody2D>();
+        theRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (playerControlled) PlayerControlledMovement();
+        else RandomMovement();
+    }
+
+    void PlayerControlledMovement()
+    {
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
         theRigidBody.velocity = new Vector2(inputX * walkSpeed, inputY * walkSpeed);
+    }
+
+    void RandomMovement()
+    {
+        if(Random.Range(0.0f,1.0f)< newDesiredVector2Threshold)
+        {
+            desiredVector2 = new Vector2(Random.Range(-1.0f,1.0f)*1000,Random.Range(-1.0f,1.0f)*1000);
+        }
+        transform.position = Vector3.MoveTowards(transform.position, desiredVector2, walkSpeed*Time.deltaTime);
     }
 }
