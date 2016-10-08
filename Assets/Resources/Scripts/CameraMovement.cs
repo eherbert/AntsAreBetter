79c55c726a70ec2d13ae2bab575baaa0432039df
Emily Herbert <eherbert@trinity.edu>
2016-10-07 23:06:15 -0500

    mouse scroll wheel facilitates zoom

---
+++ Assets/Resources/Scripts/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+    public float ROTSpeed;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ScrollWheelZoom();
+    }
+
+    void ScrollWheelZoom() {
+        float input = Input.GetAxis("Mouse ScrollWheel");
+        if(input>0 && gameObject.transform.position.z<-1) gameObject.transform.Translate(0, 0, input * ROTSpeed);
+        else if(input<0) gameObject.transform.Translate(0, 0, input * ROTSpeed);
+    }
+}
