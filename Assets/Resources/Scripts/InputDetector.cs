ec85f1ca2c85e16a1b8ed85c71fc300a8279b499
Emily Herbert <eherbert@trinity.edu>
2016-10-20 12:36:35 -0500

    added sugar cube

--- Assets/Resources/Scripts/InputDetector.cs
+++ Assets/Resources/Scripts/InputDetector.cs
@@ -5,15 +5,11 @@ public class InputDetector : MonoBehaviour {
 
     public GameObject canvas;
     public GameObject waterPrefab;
+    public GameObject sugarCubePrefab;
     public GameObject repo;
     public GameObject mainCamera;
 
     private GameObject tmp;
-
-	// Use this for initialization
-	void Start () {
-
-	}
 
     // Update is called once per frame
     void Update() {
@@ -22,6 +18,10 @@ public class InputDetector : MonoBehaviour {
                 Vector3 posVec = Input.mousePosition;
                 posVec.z = (0-Camera.main.transform.position.z)+1;
                 tmp = Instantiate(waterPrefab, Camera.main.ScreenToWorldPoint(posVec), Quaternion.identity) as GameObject;
+            } else if (Input.GetKey("s")) {
+                Vector3 posVec = Input.mousePosition;
+                posVec.z = (0 - Camera.main.transform.position.z) + 1;
+                tmp = Instantiate(sugarCubePrefab, Camera.main.ScreenToWorldPoint(posVec), Quaternion.identity) as GameObject;
             }
         } else if(Input.GetMouseButtonDown(1)) {
             Vector3 posVec = Input.mousePosition;
